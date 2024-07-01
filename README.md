40e35da4bbcd5798a31c9bff5abdd37a5ebc67ca
Lakshay Lalia <lakshaylalia@gmail.com>
2024-07-01 22:05:30 +0530

    Added basic stucture of website along with functioning

---
+++ README.md
@@ -0,0 +1,2 @@
+# README
+Weather forecast app using HTML , CSS AND JS. Uses weather API to fetch the details.  
---
+++ app.js
@@ -0,0 +1,2 @@
+let url=`http://api.weatherapi.com/v1/future.json?key=12aba9c690b94f0e9b0110501240107&q=
+&dt=2024-07-31&aqi=no&alerts=no`
