7b102fd3662a4d2a92267666322e0c00a7b4f3b9
Lakshay Lalia <lakshaylalia@gmail.com>
2024-07-03 09:21:28 +0530

    Added new feature

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-let url=`http://api.weatherapi.com/v1/future.json?key=12aba9c690b94f0e9b0110501240107&q=
-&dt=2024-07-31&aqi=no&alerts=no`
+ let body=document.querySelector("body");
+ async function weatherCurr(region){
+    let url=`http://api.weatherapi.com/v1/current.json?key=12aba9c690b94f0e9b0110501240107&q=${region}&aqi=yes`;
+    try{
+    let response =await axios.get(url);
+    return response.data.current.condition.text
+    }
+    catch(error){
+        location.reload();
+        return "Location not found"
+    }
+ }
+
+btn=document.querySelector("button")
+
+btn.addEventListener("click",async ()=>{
+    region=document.getElementById("city").value
+    let result=await weatherCurr(region);
+    alert(result)
+    document.getElementById("city").value="";
+})
+
+let reset=document.getElementById("reset");
+reset.addEventListener("click",()=>{
+    document.getElementById("city").value="";
+})
+
